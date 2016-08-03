55876c566bceece08c06c92ae8cecea669c14283
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2016-08-03 15:18:34 +0300

    initial commit

---
+++ Framework/Swissors/Swissors.h
@@ -0,0 +1,19 @@
+//
+//  Swissors.h
+//  Swissors
+//
+//  Created by Ilya Kulebyakin on 8/3/16.
+//  Copyright © 2016 e-Legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swissors.
+FOUNDATION_EXPORT double SwissorsVersionNumber;
+
+//! Project version string for Swissors.
+FOUNDATION_EXPORT const unsigned char SwissorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swissors/PublicHeader.h>
+
+
---
+++ Framework/SwissorsTests/SwissorsTests.swift
@@ -0,0 +1,14 @@
+//
+//  SwissorsTests.swift
+//  SwissorsTests
+//
+//  Created by Ilya Kulebyakin on 8/3/16.
+//  Copyright © 2016 e-Legion. All rights reserved.
+//
+
+import XCTest
+@testable import Swissors
+
+class SwissorsTests: XCTestCase {
+    
+}
