2b947948e9f048bef98e146383534a2ea98836d2
Nikolay Ischuk <ischuknn@gmail.com>
2016-12-14 16:39:31 +0300

    More swifty parameters and unnecessary cast removed.

--- Source/Bundle+Swissors.swift
+++ Source/Bundle+Swissors.swift
@@ -10,17 +10,17 @@ import Foundation
 
 public extension Bundle {
     
-    public subscript(key: String) -> AnyObject? {
+    public subscript(key: String) -> Any? {
         guard let infoDictionary = infoDictionary else { return nil }
-        return infoDictionary[key] as AnyObject?
+        return infoDictionary[key]
     }
     
-    public func sw_infoDictionaryStringForKey(_ key: String) -> String? {
+    public func sw_infoDictionaryString(for key: String) -> String? {
         guard let infoDictionary = infoDictionary else { return nil }
-        return infoDictionary[key] as! String?
+        return infoDictionary[key] as? String
     }
     
-    public func sw_infoDictionaryBoolForKey(_ key: String) -> Bool {
+    public func sw_infoDictionaryBool(for key: String) -> Bool {
         guard let infoDictionary = infoDictionary else { return false }
         guard let value = infoDictionary[key] as? String else { return false }
         return Bool(value) == true
--- Source/UIView+Swissors.swift
+++ Source/UIView+Swissors.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 public extension UIView {
-    public class func sw_viewFromNib(bundle: Bundle = Bundle.main, owner: Any? = nil, options: [AnyHashable : Any]? = nil) -> Self {
-        return sw_viewFromNibHelper(bundle: bundle, owner: owner, options: options)
+    public class func sw_viewFromNib(in bundle: Bundle = Bundle.main, owner: Any? = nil, options: [AnyHashable : Any]? = nil) -> Self {
+        return sw_viewFromNibHelper(in: bundle, owner: owner, options: options)
     }
     
-    private class func sw_viewFromNibHelper<T>(bundle: Bundle, owner: Any?, options: [AnyHashable : Any]?) -> T {
+    private class func sw_viewFromNibHelper<T>(in bundle: Bundle, owner: Any?, options: [AnyHashable : Any]?) -> T {
         let className = String(describing: self)
         let nib = UINib(nibName: className, bundle: bundle)
         return nib.instantiate(withOwner: owner, options: options).first as! T
