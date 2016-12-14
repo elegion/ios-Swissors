0d16b41babd7b898ce4ed5250a36a71cf6053dbb
Nikolay Ischuk <ischuknn@gmail.com>
2016-12-14 16:58:28 +0300

    Changing root view controller of window with animation

---
+++ Source/UIWindow+Swissors.swift
@@ -0,0 +1,32 @@
+//
+//  UIWindow+Swissors.swift
+//  Swissors
+//
+//  Created by Nikolay Ischuk on 14.12.16.
+//  Copyright © 2016 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+public extension UIWindow {
+    
+    public func changeRootViewControllerTo(_ viewController: UIViewController, withTransitionStyle transitionStyle: UIViewAnimationOptions = .transitionCrossDissolve, duration: TimeInterval = CATransaction.animationDuration()) {
+        if rootViewController == nil || transitionStyle == UIViewAnimationOptions() {
+            rootViewController = viewController
+            
+            return
+        }
+        
+        let snapshot = rootViewController!.view.snapshotView(afterScreenUpdates: false)
+        
+        if let snapshot = snapshot {
+            viewController.view.addSubview(snapshot)
+        }
+        
+        rootViewController = viewController
+        
+        UIView.transition(with: self, duration: duration, options: transitionStyle, animations: {
+            snapshot?.removeFromSuperview()
+        }, completion: nil)
+    }
+}
