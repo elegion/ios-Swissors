417d4dad68d9aa96c06dc5e8d36963ffc74d18db
Nikolay Ischuk <ischuknn@gmail.com>
2016-12-14 16:56:38 +0300

    Adding subview controller

---
+++ Source/UIViewController+Swissors.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewController+Swissors.swift
+//  Swissors
+//
+//  Created by Nikolay Ischuk on 14.12.16.
+//  Copyright © 2016 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func addSubViewController(_ subViewController: UIViewController, viewSetupClosure: ((_ superView: UIView, _ subView: UIView) -> Void)?) {
+        self.addChildViewController(subViewController)
+        
+        if let viewSetupClosure = viewSetupClosure {
+            viewSetupClosure(self.view, subViewController.view)
+        }
+        
+        subViewController.didMove(toParentViewController: self)
+    }
+    
+    func autoAddSubViewController(_ subViewController: UIViewController, constraintsSetupClosure: @escaping (_ superView: UIView, _ subView: UIView) -> [NSLayoutConstraint]) {
+        addSubViewController(subViewController) { (superView, subView) in
+            superView.addSubview(subView)
+            
+            subView.autoresizingMask = UIViewAutoresizing()
+            subView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate(constraintsSetupClosure(superView, subView))
+        }
+    }
+}
