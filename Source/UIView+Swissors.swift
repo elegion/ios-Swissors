b974589c5af0051148c028f594c7303c7d2f81f6
Nikolay Ischuk <ischuknn@gmail.com>
2016-12-14 16:52:05 +0300

    Constraints extension functions

--- Source/UIView+Swissors.swift
+++ Source/UIView+Swissors.swift
@@ -18,4 +18,52 @@ public extension UIView {
         let nib = UINib(nibName: className, bundle: bundle)
         return nib.instantiate(withOwner: owner, options: options).first as! T
     }
+    
+    public func sw_addSubview(_ subview: UIView, with constraints:[NSLayoutConstraint]) {
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(subview)
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    public static func sw_fullFrameConstraints(for superView: UIView, subView: UIView) -> [NSLayoutConstraint] {
+        if #available(iOS 9.0, *) {
+            return [subView.topAnchor.constraint(equalTo: superView.topAnchor),
+                    subView.bottomAnchor.constraint(equalTo: superView.bottomAnchor),
+                    subView.leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+                    subView.trailingAnchor.constraint(equalTo: superView.trailingAnchor)]
+        } else {
+            return [NSLayoutConstraint(item: subView,
+                    attribute: .top,
+                    relatedBy: .equal,
+                    toItem: superView,
+                    attribute: .top,
+                    multiplier: 1.0,
+                    constant: 0),
+                
+                NSLayoutConstraint(item: subView,
+                    attribute :.left,
+                    relatedBy: .equal,
+                    toItem: superView,
+                    attribute: .left,
+                    multiplier: 1.0,
+                    constant: 0),
+                
+                NSLayoutConstraint(item: subView,
+                    attribute: .bottom,
+                    relatedBy: .equal,
+                    toItem: superView,
+                    attribute: .bottom,
+                    multiplier: 1.0,
+                    constant: 0),
+                
+                NSLayoutConstraint(item: subView,
+                    attribute: .right,
+                    relatedBy: .equal,
+                    toItem: superView,
+                    attribute: .right,
+                    multiplier: 1,
+                    constant: 0)
+                ]
+        }
+    }
 }
