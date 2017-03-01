fb41173d4139ec408cf69461db837be3e7711adc
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-03-01 11:29:58 +0300

    Did changed Int & FloatingPoint properties access rights

--- Source/BaseTypes+Swissors.swift
+++ Source/BaseTypes+Swissors.swift
@@ -9,10 +9,11 @@
 import Foundation
 
 extension Int {
-    var degreesToRadians: Double { return Double(self) * .pi / 180 }
-    var radiansToDegrees: Double { return Double(self) * 180 / .pi }
+    public var degreesToRadians: Double { return Double(self) * .pi / 180 }
+    public var radiansToDegrees: Double { return Double(self) * 180 / .pi }
 }
+
 extension FloatingPoint {
-    var degreesToRadians: Self { return self * .pi / 180 }
-    var radiansToDegrees: Self { return self * 180 / .pi }
+    public var degreesToRadians: Self { return self * .pi / 180 }
+    public var radiansToDegrees: Self { return self * 180 / .pi }
 }
