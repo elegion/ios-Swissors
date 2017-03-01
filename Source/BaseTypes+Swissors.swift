1af57f8cbe36fc2d78577cbeae3baaff0a25f65a
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-03-01 23:19:57 +0300

    Reused Floatingpoint extentions

--- Source/BaseTypes+Swissors.swift
+++ Source/BaseTypes+Swissors.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-public extension Int {
-    public var degreesToRadians: Double { return Double(self) * .pi / 180 }
-    public var radiansToDegrees: Double { return Double(self) * 180 / .pi }
-}
-
 public extension FloatingPoint {
     public var degreesToRadians: Self { return self * .pi / 180 }
     public var radiansToDegrees: Self { return self * 180 / .pi }
+}
+
+public extension Int {
+    public var degreesToRadians: Double { return Double(self).degreesToRadians }
+    public var radiansToDegrees: Double { return Double(self).radiansToDegrees }
 }
