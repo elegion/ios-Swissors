b05b6694eeae51f353f2cea3ea7da8230e48f4b7
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-03-01 23:08:48 +0300

    Rearranged methods

--- Source/UIView+Swissors.swift
+++ Source/UIView+Swissors.swift
@@ -9,21 +9,8 @@
 import UIKit
 
 public extension UIView {
-    public class func sw_viewFromNib(in bundle: Bundle = Bundle.main, owner: Any? = nil, options: [AnyHashable : Any]? = nil) -> Self {
-        return sw_viewFromNibHelper(in: bundle, owner: owner, options: options)
-    }
     
-    private class func sw_viewFromNibHelper<T>(in bundle: Bundle, owner: Any?, options: [AnyHashable : Any]? = nil) -> T {
-        let className = String(describing: self)
-        let nib = UINib(nibName: className, bundle: bundle)
-        return nib.instantiate(withOwner: owner, options: options).first as! T
-    }
-    
-    public func sw_addSubview(_ subview: UIView, with constraints:[NSLayoutConstraint]) {
-        subview.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(subview)
-        NSLayoutConstraint.activate(constraints)
-    }
+    //MARK: - Public
     
     public static func sw_fullFrameConstraints(for superView: UIView, subView: UIView) -> [NSLayoutConstraint] {
         if #available(iOS 9.0, *) {
@@ -33,42 +20,60 @@ public extension UIView {
                     subView.trailingAnchor.constraint(equalTo: superView.trailingAnchor)]
         } else {
             return [NSLayoutConstraint(item: subView,
-                    attribute: .top,
-                    relatedBy: .equal,
-                    toItem: superView,
-                    attribute: .top,
-                    multiplier: 1.0,
-                    constant: 0),
-                
-                NSLayoutConstraint(item: subView,
-                    attribute :.left,
-                    relatedBy: .equal,
-                    toItem: superView,
-                    attribute: .left,
-                    multiplier: 1.0,
-                    constant: 0),
-                
-                NSLayoutConstraint(item: subView,
-                    attribute: .bottom,
-                    relatedBy: .equal,
-                    toItem: superView,
-                    attribute: .bottom,
-                    multiplier: 1.0,
-                    constant: 0),
-                
-                NSLayoutConstraint(item: subView,
-                    attribute: .right,
-                    relatedBy: .equal,
-                    toItem: superView,
-                    attribute: .right,
-                    multiplier: 1,
-                    constant: 0)
-                ]
+                                       attribute: .top,
+                                       relatedBy: .equal,
+                                       toItem: superView,
+                                       attribute: .top,
+                                       multiplier: 1.0,
+                                       constant: 0),
+                    
+                    NSLayoutConstraint(item: subView,
+                                       attribute :.left,
+                                       relatedBy: .equal,
+                                       toItem: superView,
+                                       attribute: .left,
+                                       multiplier: 1.0,
+                                       constant: 0),
+                    
+                    NSLayoutConstraint(item: subView,
+                                       attribute: .bottom,
+                                       relatedBy: .equal,
+                                       toItem: superView,
+                                       attribute: .bottom,
+                                       multiplier: 1.0,
+                                       constant: 0),
+                    
+                    NSLayoutConstraint(item: subView,
+                                       attribute: .right,
+                                       relatedBy: .equal,
+                                       toItem: superView,
+                                       attribute: .right,
+                                       multiplier: 1,
+                                       constant: 0)
+            ]
         }
+    }
+    
+    public class func sw_viewFromNib(in bundle: Bundle = Bundle.main, owner: Any? = nil, options: [AnyHashable : Any]? = nil) -> Self {
+        return sw_viewFromNibHelper(in: bundle, owner: owner, options: options)
+    }
+    
+    public func sw_addSubview(_ subview: UIView, with constraints:[NSLayoutConstraint]) {
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(subview)
+        NSLayoutConstraint.activate(constraints)
     }
     
     public func sw_setCornerRadius(_ radius: CGFloat) {
         layer.cornerRadius = radius
         layer.masksToBounds = radius > 0
+    }
+    
+    //MARK: - Private
+    
+    private class func sw_viewFromNibHelper<T>(in bundle: Bundle, owner: Any?, options: [AnyHashable : Any]? = nil) -> T {
+        let className = String(describing: self)
+        let nib = UINib(nibName: className, bundle: bundle)
+        return nib.instantiate(withOwner: owner, options: options).first as! T
     }
 }
