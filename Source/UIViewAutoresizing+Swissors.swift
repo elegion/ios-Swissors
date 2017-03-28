6a18dbe327abb6136f6bb6fa9a6594fb75250837
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-03-28 19:30:31 +0300

    Implemented UIViewAutoresizing extention

---
+++ Source/UIViewAutoresizing+Swissors.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewAutoresizing+Swissors.swift
+//  Swissors
+//
+//  Created by Evgeniy Akhmerov on 28/03/17.
+//  Copyright © 2017 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+public extension UIViewAutoresizing {
+    
+    static var sw_flexibleMargins: UIViewAutoresizing {
+        return [self.flexibleLeftMargin, self.flexibleTopMargin, self.flexibleRightMargin, self.flexibleBottomMargin]
+    }
+    
+    static var sw_flexibleSize: UIViewAutoresizing {
+        return [self.flexibleWidth, self.flexibleHeight]
+    }
+}
