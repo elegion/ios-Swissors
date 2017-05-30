ae78015189b92b3e882f825cbdcd9391b4276588
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2017-05-30 15:25:56 +0300

    Implement NSLayoutConstraint helper method to specify constraint priority

---
+++ Source/NSLayoutConstraint+Swissors.swift
@@ -0,0 +1,18 @@
+//
+//  NSLayoutConstraint+Swissors.swift
+//  Swissors
+//
+//  Created by Ilya Kulebyakin on 5/30/17.
+//  Copyright © 2017 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+extension NSLayoutConstraint {
+    
+    func sw_with(priority: UILayoutPriority) -> NSLayoutConstraint {
+        self.priority = priority
+        return self
+    }
+    
+}
