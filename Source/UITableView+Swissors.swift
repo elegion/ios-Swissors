1bce740788ea80397ab66a21039cf81e4dcdc6e6
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2017-05-30 13:29:50 +0300

    Update UITableView registering/dequeuing helper methods

--- Source/UITableView+Swissors.swift
+++ Source/UITableView+Swissors.swift
@@ -10,29 +10,69 @@ import UIKit
 
 public extension UITableView {
     
-    public func sw_registerReusableCellOfType<T: UITableViewCell>(_ cellType: T.Type) {
+    func sw_register<Cell: UITableViewCell>(cellType: Cell.Type, bundle: Bundle = .main, tryNib: Bool = true) {
+        let reuseIdentifier = String(describing: cellType)
+        
+        if tryNib && bundle.path(forResource: reuseIdentifier, ofType: "nib") != nil {
+            register(UINib(nibName: reuseIdentifier, bundle: bundle), forCellReuseIdentifier: reuseIdentifier)
+        } else {
+            register(cellType, forCellReuseIdentifier: reuseIdentifier)
+        }
+    }
+    
+    func sw_dequeueCell<Cell: UITableViewCell>(of cellType: Cell.Type, for indexPath: IndexPath) -> Cell {
+        return dequeueReusableCell(withIdentifier: String(describing: cellType), for: indexPath) as! Cell
+    }
+    
+    func sw_register<HeaderFooter: UITableViewHeaderFooterView>(headerFooterType: HeaderFooter.Type, bundle: Bundle = .main, tryNib: Bool = true) {
+        let identifier = String(describing: headerFooterType)
+        
+        if tryNib && bundle.path(forResource: identifier, ofType: "nib") != nil {
+            register(UINib(nibName: identifier, bundle: bundle), forHeaderFooterViewReuseIdentifier: identifier)
+        } else {
+            register(headerFooterType, forHeaderFooterViewReuseIdentifier: identifier)
+        }
+    }
+    
+    func sw_dequeueHeaderFooter<HeaderFooter: UITableViewHeaderFooterView>(of viewType: HeaderFooter.Type) -> HeaderFooter {
+        return dequeueReusableHeaderFooterView(withIdentifier: String(describing: viewType)) as! HeaderFooter
+    }
+    
+}
+
+// MARK: Deprecated
+
+public extension UITableView {
+    
+    @available(*, deprecated, message: "Use sw_register<Cell: UITableViewCell>(cellType: Cell.Type, bundle: Bundle, tryNib: Bool) instead")
+    func sw_registerReusableCellOfType<T: UITableViewCell>(_ cellType: T.Type) {
         register(cellType, forCellReuseIdentifier: String(describing: cellType))
     }
     
-    public func sw_registerReusableCellNibOfType<T: UITableViewCell>(_ cellType: T.Type, bundle: Bundle? = nil) {
+    @available(*, deprecated, message: "Use sw_register<Cell: UITableViewCell>(cellType: Cell.Type, bundle: Bundle, tryNib: Bool) instead")
+    func sw_registerReusableCellNibOfType<T: UITableViewCell>(_ cellType: T.Type, bundle: Bundle? = nil) {
         let nib = UINib(nibName: String(describing: cellType), bundle: bundle)
         register(nib, forCellReuseIdentifier: String(describing: cellType))
     }
     
-    public func sw_dequeueReusableCellOfType<T: UITableViewCell>(_ cellType: T.Type, forIndexPath indexPath: IndexPath) -> T {
+    @available(*, deprecated, message: "Use sw_dequeueCell<Cell: UITableViewCell>(of cellType: Cell.Type, for indexPath: IndexPath) -> Cell instead")
+    func sw_dequeueReusableCellOfType<T: UITableViewCell>(_ cellType: T.Type, forIndexPath indexPath: IndexPath) -> T {
         return dequeueReusableCell(withIdentifier: String(describing: cellType), for: indexPath) as! T
     }
     
-    public func sw_registerReusableHeaderFooterViewOfType<T: UITableViewHeaderFooterView>(_ viewType: T.Type) {
+    @available(*, deprecated, message: "Use sw_register<HeaderFooter: UITableViewHeaderFooterView>(headerFooterType: HeaderFooter.Type, bundle: Bundle, tryNib: Bool) instead")
+    func sw_registerReusableHeaderFooterViewOfType<T: UITableViewHeaderFooterView>(_ viewType: T.Type) {
         register(viewType, forHeaderFooterViewReuseIdentifier: String(describing: viewType))
     }
     
-    public func sw_registerHeaderFooterViewNibOfType<T: UITableViewHeaderFooterView>(_ viewType: T.Type, bundle: Bundle? = nil) {
+    @available(*, deprecated, message: "Use sw_register<HeaderFooter: UITableViewHeaderFooterView>(headerFooterType: HeaderFooter.Type, bundle: Bundle, tryNib: Bool) instead")
+    func sw_registerHeaderFooterViewNibOfType<T: UITableViewHeaderFooterView>(_ viewType: T.Type, bundle: Bundle? = nil) {
         let nib = UINib(nibName: String(describing: viewType), bundle: bundle)
         register(nib, forHeaderFooterViewReuseIdentifier: String(describing: viewType))
     }
     
-    public func sw_dequeueReusableHeaderFooterViewOfType<T: UITableViewHeaderFooterView>(_ viewType: T.Type) -> T {
+    @available(*, deprecated, message: "sw_dequeueHeaderFooter<HeaderFooter: UITableViewHeaderFooterView>(of viewType: HeaderFooter.Type) -> HeaderFooter")
+    func sw_dequeueReusableHeaderFooterViewOfType<T: UITableViewHeaderFooterView>(_ viewType: T.Type) -> T {
         return dequeueReusableHeaderFooterView(withIdentifier: String(describing: viewType)) as! T
     }
 }
