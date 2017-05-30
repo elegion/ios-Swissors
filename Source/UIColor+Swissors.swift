bef9730fbcb54583d03cb00b21b2e167570a034c
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2017-05-30 16:02:33 +0300

    implement UIColor helper initializers and methods

--- Source/UIColor+Swissors.swift
+++ Source/UIColor+Swissors.swift
@@ -10,6 +10,8 @@ import Foundation
 
 public extension UIColor {
     
+    typealias RGBA = (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+    
     public convenience init(hex: UInt32) {
         let r = CGFloat((hex & 0xFF0000) >> 16) / 255;
         let g = CGFloat((hex & 0x00FF00) >> 8) / 255;
@@ -28,6 +30,22 @@ public extension UIColor {
         } else {
             return nil
         }
+    }
+    
+    public convenience init(absoluteRed red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
+    }
+    
+    public convenience init(rgba: RGBA) {
+        self.init(red: rgba.red, green: rgba.green, blue: rgba.blue, alpha: rgba.alpha)
+    }
+    
+    public var sw_rgba: RGBA {
+        var result: RGBA = (0.0, 0.0, 0.0, 0.0)
+        
+        getRed(&(result.red), green: &(result.green), blue: &(result.blue), alpha: &(result.alpha))
+        
+        return result
     }
     
     public var sw_hex: String? {
@@ -71,6 +89,33 @@ public extension UIColor {
         let a = a1 * beta + a2 * alpha
         
         return UIColor(red: r, green: g, blue: b, alpha: a)
+    }
+    
+    public func sw_intermediate(to other: UIColor, atPercent percent: CGFloat) -> UIColor {
+        
+        let selfRGBA = sw_rgba
+        let otherRGBA = other.sw_rgba
+        
+        func intermediate(from start: CGFloat, to end: CGFloat, by percent: CGFloat) -> CGFloat {
+            return start + (end - start) * percent
+        }
+        
+        var newRGBA: RGBA
+        
+        newRGBA.red = intermediate(from: selfRGBA.red, to: otherRGBA.red, by: percent)
+        newRGBA.green = intermediate(from: selfRGBA.green, to: otherRGBA.green, by: percent)
+        newRGBA.blue = intermediate(from: selfRGBA.blue, to: otherRGBA.blue, by: percent)
+        newRGBA.alpha = intermediate(from: selfRGBA.alpha, to: otherRGBA.alpha, by: percent)
+        
+        return UIColor(rgba: newRGBA)
+    }
+    
+}
+
+public extension CGColor {
+    
+    var sw_uiColor: UIColor? {
+        return UIColor(cgColor: self)
     }
     
 }
