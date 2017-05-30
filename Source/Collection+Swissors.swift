c179fd63e823067be0c343c5b2726eb152d97e29
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2017-05-30 14:56:34 +0300

    Implement collection categorization method

---
+++ Source/Collection+Swissors.swift
@@ -0,0 +1,30 @@
+//
+//  Collection+Swissors.swift
+//  Swissors
+//
+//  Created by Ilya Kulebyakin on 5/30/17.
+//  Copyright © 2017 e-Legion. All rights reserved.
+//
+
+import Foundation
+
+extension Collection {
+    
+    func categorize<Category: Hashable>(where categorize: (Iterator.Element) throws -> Category) rethrows -> [Category: [Iterator.Element]] {
+        var result: [Category: [Iterator.Element]] = [:]
+        
+        for element in self {
+            let category = try categorize(element)
+            
+            if var categoryResult = result[category] {
+                categoryResult.append(element)
+                result[category] = categoryResult
+            } else {
+                result[category] = [element]
+            }
+        }
+        
+        return result
+    }
+    
+}
