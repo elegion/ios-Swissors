bdc8b287ce9511a0d7218368818c6420266044e6
Maxim Kotelevsky <maxim.kotelevsky@e-legion.com>
2020-01-27 01:22:12 +0200

    Add bundle JSON parsing

--- Source/Bundle+Swissors.swift
+++ Source/Bundle+Swissors.swift
@@ -25,4 +25,21 @@ public extension Bundle {
         guard let value = infoDictionary[key] as? String else { return defaultValue }
         return Bool(value) ?? defaultValue
     }
+    
+    func dw_decode<T: Decodable>(_ type: T.Type, from filename: String) -> T {
+        guard let json = url(forResource: filename, withExtension: nil) else {
+            fatalError("Failed to locate \(filename) in app bundle.")
+        }
+        
+        guard let jsonData = try? Data(contentsOf: json) else {
+            fatalError("Failed to load \(filename) from app bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        guard let result = try? decoder.decode(T.self, from: jsonData) else {
+            fatalError("Failed to decode \(filename) from app bundle.")
+        }
+        
+        return result
+    }
 }
