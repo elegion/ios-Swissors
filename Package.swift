766f38dbcc30ef64d22896ba34c54f8ec68af666
viktor.volkov <viktor.volkov@e-legion.com>
2020-09-29 16:18:51 +0300

    Update podspec

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Swissors",
+    products: [
+        .library(
+            name: "Swissors",
+            targets: ["Swissors"]),
+    ],
+    targets: [
+        .target(
+            name: "Swissors",
+            dependencies: []),
+        .testTarget(
+            name: "SwissorsTests",
+            dependencies: ["Swissors"]),
+    ]
+)
--- Sources/Swissors/Extensions/UINib+Swissors.swift
+++ Sources/Swissors/Extensions/UINib+Swissors.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 e-Legion. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public extension UINib {
     
