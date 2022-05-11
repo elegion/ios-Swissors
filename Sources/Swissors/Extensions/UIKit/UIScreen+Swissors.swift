153f6ad380202b01890cfe4b168f76d5a0dd4609
Roman <roman.uvarov@e-legion.com>
2022-05-11 12:49:45 +0300

    Fix keyWindow

--- Sources/Swissors/Extensions/UIKit/UIScreen+Swissors.swift
+++ Sources/Swissors/Extensions/UIKit/UIScreen+Swissors.swift
@@ -10,15 +10,28 @@ import UIKit
 extension UIScreen {
     
     static func topSafeAreaInset() -> CGFloat {
-        let insets = UIApplication.shared.windows.first?.safeAreaInsets.top ?? UIApplication.shared.statusBarFrame.height
+        let insets = getKeyWindow()?.safeAreaInsets.top ?? UIApplication.shared.statusBarFrame.height
         return insets == .zero ? UIApplication.shared.statusBarFrame.height : insets
     }
     
     static func bottomSafeAreaInset() -> CGFloat {
-        UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? .zero
+        getKeyWindow()?.safeAreaInsets.bottom ?? .zero
     }
     
     static let width = UIScreen.main.bounds.width
     static let height = UIScreen.main.bounds.height
     static let size = UIScreen.main.bounds.size
+    
+    private static func getKeyWindow() -> UIWindow? {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
 }
